2af2218c47b69a153127633237e3255b88c0860e
Prateekk02 <prateek.kumar2002@gmail.com>
2024-12-27 16:29:53 +0530

    Added Input component

--- src/components/InputComponent/Input.tsx
+++ src/components/InputComponent/Input.tsx
@@ -1,5 +1,14 @@
+interface InputProps{
+    placeholder?:string,
+    value?: string,
+    type?:string,
+    disabled?: boolean,
+    onChange: (event: React.ChangeEvent<HTMLInputElement>) => void    
+}
+
 const Input = () => {
     return <>
+
     </>
 }
 

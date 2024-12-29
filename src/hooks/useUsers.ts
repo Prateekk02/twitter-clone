85e414f087059c3c9254f0201ecfb6c4a79585c3
Prateekk02 <prateek.kumar2002@gmail.com>
2024-12-29 20:33:09 +0530

    Add useUsers and useUser hooks

---
+++ src/hooks/useUsers.ts
@@ -0,0 +1,15 @@
+import useSWR from "swr";
+import fetcher from "@/lib/fetcher";
+
+const useUsers = () =>{
+    const {data, error, isLoading, mutate} =  useSWR('/api/user', fetcher); 
+
+    return {
+        data,
+        error,
+        isLoading,
+        mutate
+    }
+}
+
+export default useUsers;
